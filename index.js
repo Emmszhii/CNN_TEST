cc7e1acb86808a1518e9a081fdb97f559decad2e
Emmszhii <emmanuelmanalo707@gmail.com>
2023-01-23 01:28:30 +0800

    Single Reference Image compare to 10 images

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const uploadPicture = (event) => {
     queryFiles.push(fileList[i]);
   }
   // logFiles();
-  if (referenceFile && queryFiles.length > 1) {
+  if (referenceFile.length > 0 && queryFiles.length > 0) {
     compareImage();
   } else {
     console.log(`Please check if there is no faces uploaded`);
@@ -44,52 +44,105 @@ const logFiles = () => {
   }
 };
 
-const compareImage = async () => {
+const setDescriptor = async (arr, option) => {
   const options = new faceapi.TinyFaceDetectorOptions({
     inputSize: 128,
     scoreThreshold: 0.5,
   });
-  try {
-    const refImg = new Image();
-    refImg.src = URL.createObjectURL(referenceFile[0]);
-    const refImage = await faceapi
-      .detectSingleFace(refImg, options)
+  for (let i = 0; i < arr.length; i++) {
+    const img = new Image();
+    img.src = URL.createObjectURL(arr[i]);
+    const image = await faceapi
+      .detectSingleFace(img, options)
       .withFaceLandmarks(true)
       .withFaceDescriptor();
-    if (!refImage) console.log(`Reference image invalid`);
-    if (refImage)
-      console.log(`Reference Image ${referenceFile[0].name} Added `);
 
-    for (let i = 0; i < queryFiles.length; i++) {
-      let queryImg = new Image();
-      queryImg.src = URL.createObjectURL(queryFiles[i]);
-
-      const queryImage = await faceapi
-        .detectSingleFace(queryImg, options)
-        .withFaceLandmarks(true)
-        .withFaceDescriptor();
-
-      if (!queryImage) {
-        console.log(`File ${queryFiles[i].name} face does not recognize`);
+    if (option === 'reference') {
+      if (!image) {
+        console.log(`Reference Image ${i + 1}: ${arr[i].name} face invalid`);
       } else {
-        console.log(`Query Image ${i + 1}: ${queryFiles[i].name} Added`);
-
-        const distance = faceapi.euclideanDistance(
-          refImage.descriptor,
-          queryImage.descriptor
+        console.log(
+          `Reference Image ${i + 1}: ${arr[0].name} descriptor Added `
         );
-        console.log(distance);
-        if (distance <= 0.43) {
-          console.log(
-            `Reference ${referenceFile[0].name} is the same with ${queryFiles[i].name}`
-          );
-        } else {
-          console.log(
-            `Reference ${referenceFile[0].name} is not the same with ${queryFiles[i].name}`
-          );
-        }
+        arr[i].descriptor = image.descriptor;
       }
     }
+    if (option === 'query') {
+      if (!image) {
+        console.log(`Query Image ${i + 1}: ${arr[i].name} face invalid`);
+      } else {
+        console.log(`Query Image ${i + 1}: ${arr[i].name} descriptor Added`);
+        arr[i].descriptor = image.descriptor;
+      }
+    }
+  }
+  console.log(arr);
+};
+const compareDescriptor = async (refArr, queryArr) => {
+  const refDescriptor = [];
+  const queryDescriptor = [];
+  refArr.map((item, i) => {
+    if (item.descriptor) {
+      refDescriptor.push(item.descriptor);
+      console.log(`Reference Image Descriptor ${i}: ${item.name} added`);
+    }
+  });
+  // const totalReferenceImg = await computeEuclideanDistance(refDescriptor);
+  queryArr.map((item, i) => {
+    if (item.descriptor) {
+      queryDescriptor.push(item.descriptor);
+      const distance = faceapi.euclideanDistance(
+        refDescriptor[0],
+        item.descriptor
+      );
+      if (distance <= 0.43) {
+        console.log(
+          `Reference Image and Query Image ${i + 1}: is the same person`
+        );
+      } else {
+        console.log(
+          `Reference Image and Query Image ${i + 1}: is the NOT same person`
+        );
+      }
+    }
+  });
+};
+
+const computeEuclideanDistance = async (arr) => {
+  let run = 0;
+  let distance;
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      console.log(i);
+      distance = faceapi.euclideanDistance(arr[i], arr[i + 1]);
+      run++;
+      total = distance;
+      console.log(total);
+    } else if (i < arr.length - 2) {
+      console.log(i);
+      const dis = faceapi.euclideanDistance(arr[i + 1], arr[i + 2]);
+      run++;
+      total = total + dis / 2;
+      console.log(total);
+    } else if (i === arr.length - 1) {
+      console.log(i);
+      const dis = faceapi.euclideanDistance(arr[i - 1], arr[i]);
+      run++;
+      total = total + dis / 2;
+      console.log(total);
+    }
+  }
+  console.log(run);
+  console.log(`Total Euclidean distance: ${total}`);
+  return total;
+};
+
+const compareImage = async () => {
+  try {
+    await setDescriptor(referenceFile, 'reference');
+    await setDescriptor(queryFiles, 'query');
+    await compareDescriptor(referenceFile, queryFiles);
   } catch (e) {
     console.log(e);
   }
