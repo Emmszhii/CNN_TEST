8259b5ef2eb7f8760d61b4a4ab48425feda7abb2
Emmszhii <emmanuelmanalo707@gmail.com>
2023-01-24 03:16:09 +0800

    optmized testing cnn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const onePersonCheckbox = document.getElementById('one__person__checkbox');
+const diffPersonCheckbox = document.getElementById('diff__person__checkbox');
 const options = new faceapi.TinyFaceDetectorOptions({
   inputSize: 128,
   scoreThreshold: 0.5,
@@ -7,6 +8,7 @@ const referenceFile = [];
 const queryFiles = [];
 const referenceDescriptor = [];
 const queryDescriptor = [];
+const distanceThreshold = 0.43;
 
 const getReferenceFiles = (event) => {
   const file = event.target.files;
@@ -29,13 +31,45 @@ const getQueryFiles = async (event) => {
   // logFiles();
   if (onePersonCheckbox.checked) {
     await onePersonImageHandler();
-  } else if (referenceFile.length > 0 && queryFiles.length > 0) {
-    compareImage();
+  } else if (diffPersonCheckbox.checked) {
+    await diffPersonHandler();
   } else {
-    console.log(`Please check if there is no file uploaded`);
-    console.log(referenceFile);
-    console.log(queryFiles);
+    console.log(`Please select a checkbox`);
   }
+
+  // if (onePersonCheckbox.checked) {
+  // } else if (referenceFile.length > 0 && queryFiles.length > 0) {
+  //   compareImage();
+  // } else {
+  //   console.log(`Please check if there is no file uploaded`);
+  //   console.log(referenceFile);
+  //   console.log(queryFiles);
+  // }
+};
+
+const diffPersonHandler = async () => {
+  await setRefDescriptor();
+  await setQueryDescriptor();
+
+  referenceDescriptor.map((ref, i) => {
+    const labelRefPerson = new faceapi.LabeledFaceDescriptors(ref.name, [
+      ref.descriptor,
+    ]);
+    const faceMatcher = new faceapi.FaceMatcher(
+      [labelRefPerson],
+      distanceThreshold
+    );
+    console.log(faceMatcher);
+    queryDescriptor.map((query, j) => {
+      const result = faceMatcher.findBestMatch(query.descriptor);
+
+      console.log(
+        `Reference ${i + 1} ${ref.name} is ${result.toString()} to ${j + 1} ${
+          query.name
+        }`
+      );
+    });
+  });
 };
 
 const setRefDescriptor = async () => {
@@ -92,14 +126,17 @@ const onePersonCompare = async () => {
     referenceDescriptor[0].name,
     refArrDescriptor
   );
-  const faceMatcher = new faceapi.FaceMatcher([labelRefPerson]);
+  const faceMatcher = new faceapi.FaceMatcher(
+    [labelRefPerson],
+    distanceThreshold
+  );
   console.log(faceMatcher);
+
   for (let i = 0; i < queryDescriptor.length; i++) {
     const bestMatch = faceMatcher.findBestMatch(queryDescriptor[i].descriptor);
     console.log(
-      `Reference is ${bestMatch.toString()} to query ${
-        queryDescriptor[i].name
-      } ${i + 1}`
+      `the reference is ${bestMatch.toString()} to query ${i + 1} 
+      ${queryDescriptor[i].name}`
     );
   }
 };
@@ -114,148 +151,148 @@ const onePersonImageHandler = async () => {
   }
 };
 
-const logFiles = () => {
-  for (let i = 0; i < referenceFile.length; i++) {
-    console.log(referenceFile[i].name);
-  }
-  for (let i = 0; i < queryFiles.length; i++) {
-    console.log(queryFiles[i].name);
-  }
-};
+// const logFiles = () => {
+//   for (let i = 0; i < referenceFile.length; i++) {
+//     console.log(referenceFile[i].name);
+//   }
+//   for (let i = 0; i < queryFiles.length; i++) {
+//     console.log(queryFiles[i].name);
+//   }
+// };
 
-const setDescriptor = async (arr, option) => {
-  for (let i = 0; i < arr.length; i++) {
-    const img = new Image();
-    img.src = URL.createObjectURL(arr[i]);
-    const image = await faceapi
-      .detectSingleFace(img, options)
-      .withFaceLandmarks(true)
-      .withFaceDescriptor();
+// const setDescriptor = async (arr, option) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     const img = new Image();
+//     img.src = URL.createObjectURL(arr[i]);
+//     const image = await faceapi
+//       .detectSingleFace(img, options)
+//       .withFaceLandmarks(true)
+//       .withFaceDescriptor();
 
-    if (option === 'reference') {
-      if (!image) {
-        console.log(`Reference Image ${i + 1}: ${arr[i].name} face invalid`);
-      } else {
-        console.log(
-          `Reference Image ${i + 1}: ${arr[i].name} descriptor Added `
-        );
-        arr[i].descriptor = image.descriptor;
-      }
-    }
-    if (option === 'query') {
-      if (!image) {
-        console.log(`Query Image ${i + 1}: ${arr[i].name} face invalid`);
-      } else {
-        console.log(`Query Image ${i + 1}: ${arr[i].name} descriptor Added`);
-        arr[i].descriptor = image.descriptor;
-      }
-    }
-  }
-  console.log(arr);
-};
+//     if (option === 'reference') {
+//       if (!image) {
+//         console.log(`Reference Image ${i + 1}: ${arr[i].name} face invalid`);
+//       } else {
+//         console.log(
+//           `Reference Image ${i + 1}: ${arr[i].name} descriptor Added `
+//         );
+//         arr[i].descriptor = image.descriptor;
+//       }
+//     }
+//     if (option === 'query') {
+//       if (!image) {
+//         console.log(`Query Image ${i + 1}: ${arr[i].name} face invalid`);
+//       } else {
+//         console.log(`Query Image ${i + 1}: ${arr[i].name} descriptor Added`);
+//         arr[i].descriptor = image.descriptor;
+//       }
+//     }
+//   }
+//   console.log(arr);
+// };
 
-const compareDescriptor = async (refArr, queryArr) => {
-  const refDescriptor = [];
+// const compareDescriptor = async (refArr, queryArr) => {
+//   const refDescriptor = [];
 
-  refArr.map((item, i) => {
-    if (item.descriptor) {
-      refDescriptor.push({ descriptor: item.descriptor, name: item.name });
+//   refArr.map((item, i) => {
+//     if (item.descriptor) {
+//       refDescriptor.push({ descriptor: item.descriptor, name: item.name });
 
-      console.log(`Reference Image Descriptor ${i}: ${item.name} added`);
-    }
-  });
-  if (refArr.length === 1) {
-    oneReferenceToManyQuery(queryArr, refDescriptor);
-  } else {
-    const queryDescriptor = queryArr.map((item) => {
-      if (item.descriptor) {
-        return { descriptor: item.descriptor, name: item.name };
-      }
-    });
+//       console.log(`Reference Image Descriptor ${i}: ${item.name} added`);
+//     }
+//   });
+//   if (refArr.length === 1) {
+//     oneReferenceToManyQuery(queryArr, refDescriptor);
+//   } else {
+//     const queryDescriptor = queryArr.map((item) => {
+//       if (item.descriptor) {
+//         return { descriptor: item.descriptor, name: item.name };
+//       }
+//     });
 
-    await computeEuclideanDistance(refDescriptor, queryDescriptor);
-  }
-};
+//     await computeEuclideanDistance(refDescriptor, queryDescriptor);
+//   }
+// };
 
-const oneReferenceToManyQuery = (arr, refDescriptor) => {
-  const queryDescriptor = [];
-  arr.map((item, i) => {
-    if (item.descriptor) {
-      queryDescriptor.push(item.descriptor);
-      const distance = faceapi.euclideanDistance(
-        refDescriptor[0],
-        item.descriptor
-      );
-      compareDistance(distance, i);
-    }
-  });
-};
+// const oneReferenceToManyQuery = (arr, refDescriptor) => {
+//   const queryDescriptor = [];
+//   arr.map((item, i) => {
+//     if (item.descriptor) {
+//       queryDescriptor.push(item.descriptor);
+//       const distance = faceapi.euclideanDistance(
+//         refDescriptor[0],
+//         item.descriptor
+//       );
+//       compareDistance(distance, i);
+//     }
+//   });
+// };
 
-const compareDistance = (distance, i) => {
-  if (distance <= 0.43) {
-    if (i) {
-      console.log(
-        `Reference Image and Query Image ${
-          i + 1
-        }: is the same person with the distance of ${distance}`
-      );
-    } else {
-      console.log(
-        `Reference Image and Query Image: is the same person with the distance of ${distance}`
-      );
-    }
-  } else {
-    if (i) {
-      console.log(
-        `Reference Image and Query Image ${
-          i + 1
-        }: is the NOT same person with the distance of ${distance}`
-      );
-    } else {
-      console.log(
-        `Reference Image and Query Image : is the NOT same person with the distance of ${distance}`
-      );
-    }
-  }
-};
+// const compareDistance = (distance, i) => {
+//   if (distance <= 0.43) {
+//     if (i) {
+//       console.log(
+//         `Reference Image and Query Image ${
+//           i + 1
+//         }: is the same person with the distance of ${distance}`
+//       );
+//     } else {
+//       console.log(
+//         `Reference Image and Query Image: is the same person with the distance of ${distance}`
+//       );
+//     }
+//   } else {
+//     if (i) {
+//       console.log(
+//         `Reference Image and Query Image ${
+//           i + 1
+//         }: is the NOT same person with the distance of ${distance}`
+//       );
+//     } else {
+//       console.log(
+//         `Reference Image and Query Image : is the NOT same person with the distance of ${distance}`
+//       );
+//     }
+//   }
+// };
 
-const computeEuclideanDistance = async (arr, arr2) => {
-  console.log(arr);
-  console.log(arr2);
+// const computeEuclideanDistance = async (arr, arr2) => {
+//   console.log(arr);
+//   console.log(arr2);
 
-  arr.map((item, i) => {
-    arr2.map((val, j) => {
-      const distance = faceapi.euclideanDistance(
-        item.descriptor,
-        val.descriptor
-      );
+//   arr.map((item, i) => {
+//     arr2.map((val, j) => {
+//       const distance = faceapi.euclideanDistance(
+//         item.descriptor,
+//         val.descriptor
+//       );
 
-      if (distance <= 0.43) {
-        console.log(
-          `Reference ${i + i} ${item.name}: is the same person with query ${
-            j + 1
-          } ${val.name} , with the distance of ${distance}`
-        );
-      } else {
-        console.log(
-          `Reference ${i + 1} ${item.name}: is NOT the same person with query ${
-            j + 1
-          } ${val.name} , with the distance of ${distance}`
-        );
-      }
-    });
-  });
-};
+//       if (distance <= 0.43) {
+//         console.log(
+//           `Reference ${i + i} ${item.name}: is the same person with query ${
+//             j + 1
+//           } ${val.name} , with the distance of ${distance}`
+//         );
+//       } else {
+//         console.log(
+//           `Reference ${i + 1} ${item.name}: is NOT the same person with query ${
+//             j + 1
+//           } ${val.name} , with the distance of ${distance}`
+//         );
+//       }
+//     });
+//   });
+// };
 
-const compareImage = async () => {
-  try {
-    await setDescriptor(referenceFile, 'reference');
-    await setDescriptor(queryFiles, 'query');
-    await compareDescriptor(referenceFile, queryFiles);
-  } catch (e) {
-    console.log(e);
-  }
-};
+// const compareImage = async () => {
+//   try {
+//     await setDescriptor(referenceFile, 'reference');
+//     await setDescriptor(queryFiles, 'query');
+//     await compareDescriptor(referenceFile, queryFiles);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
 window.addEventListener('DOMContentLoaded', () => {
   Promise.all([
@@ -271,3 +308,16 @@ window.addEventListener('DOMContentLoaded', () => {
       .addEventListener('change', getQueryFiles);
   });
 });
+
+const checkBoxes = (e) => {
+  const btn = e.currentTarget;
+  if (!btn.checked) return;
+  if (btn === onePersonCheckbox) {
+    diffPersonCheckbox.checked = false;
+  } else {
+    onePersonCheckbox.checked = false;
+  }
+};
+
+onePersonCheckbox.addEventListener('click', checkBoxes);
+diffPersonCheckbox.addEventListener('click', checkBoxes);
