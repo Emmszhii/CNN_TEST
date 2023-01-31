273eeddc12cc59fe006f6238d76fb600ac719340
Emmszhii <emmanuelmanalo707@gmail.com>
2023-01-31 22:49:11 +0800

    Knn classifier added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const onePersonCheckbox = document.getElementById('one__person__checkbox');
 const diffPersonCheckbox = document.getElementById('diff__person__checkbox');
+const cnnCheckbox = document.getElementById('face__api__js');
+const knnCheckbox = document.getElementById('knn__classifier__js');
 const options = new faceapi.TinyFaceDetectorOptions({
   inputSize: 128,
   scoreThreshold: 0.5,
 });
+
+let classifierKnn;
+let mobilenetModule;
+let tinyFaceApiNet;
 const referenceFile = [];
 const queryFiles = [];
 const referenceDescriptor = [];
@@ -28,29 +34,50 @@ const getQueryFiles = async (event) => {
   for (let i = 0; i < fileList.length; i++) {
     queryFiles.push(fileList[i]);
   }
-  // logFiles();
-  if (onePersonCheckbox.checked) {
+
+  if (cnnCheckbox.checked && onePersonCheckbox.checked) {
     await onePersonImageHandler();
-  } else if (diffPersonCheckbox.checked) {
+  } else if (cnnCheckbox.checked && diffPersonCheckbox.checked) {
+    await setRefDescriptor();
+    await setQueryDescriptor();
     await diffPersonHandler();
+  } else if (knnCheckbox.checked && onePersonCheckbox.checked) {
+    if (classifierKnn.getClassifierDataset()) classifierKnn.clearAllClasses();
+    await knnClassifierHandler();
   } else {
     console.log(`Please select a checkbox`);
   }
+};
 
-  // if (onePersonCheckbox.checked) {
-  // } else if (referenceFile.length > 0 && queryFiles.length > 0) {
-  //   compareImage();
-  // } else {
-  //   console.log(`Please check if there is no file uploaded`);
-  //   console.log(referenceFile);
-  //   console.log(queryFiles);
-  // }
+const knnClassifierHandler = async () => {
+  referenceFile.map(async (file, i) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.id = file.name;
+    document.body.append(img);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    const newImg = tf.browser.fromPixels(canvas);
+    const logits = mobilenetModule.infer(newImg, true);
+    classifierKnn.addExample(logits, file.name);
+  });
+  queryFiles.map(async (file, i) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.id = file.name;
+    document.body.append(img);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    const newImg = tf.browser.fromPixels(canvas);
+    const x = mobilenetModule.infer(newImg, true);
+    const result = await classifierKnn.predictClass(x);
+    console.log(result);
+  });
 };
 
 const diffPersonHandler = async () => {
-  await setRefDescriptor();
-  await setQueryDescriptor();
-
   referenceDescriptor.map((ref, i) => {
     const labelRefPerson = new faceapi.LabeledFaceDescriptors(ref.name, [
       ref.descriptor,
@@ -89,6 +116,9 @@ const setRefDescriptor = async () => {
     console.log(
       `Reference Image ${referenceFile[i].name} ${i + 1} face descriptor added`
     );
+    console.log(detection);
+    console.log(detection.descriptor);
+    console.log(detection.descriptor.toString());
     referenceDescriptor.push({
       name: referenceFile[i].name,
       descriptor: detection.descriptor,
@@ -110,6 +140,9 @@ const setQueryDescriptor = async () => {
     if (!detection)
       return console.log(`There are no face detected in the image`);
 
+    console.log(detection);
+    console.log(detection.descriptor);
+    console.log(detection.descriptor.toString());
     console.log(
       `Query Image ${queryFiles[i].name} ${i + 1} face descriptor added`
     );
@@ -151,161 +184,27 @@ const onePersonImageHandler = async () => {
   }
 };
 
-// const logFiles = () => {
-//   for (let i = 0; i < referenceFile.length; i++) {
-//     console.log(referenceFile[i].name);
-//   }
-//   for (let i = 0; i < queryFiles.length; i++) {
-//     console.log(queryFiles[i].name);
-//   }
-// };
-
-// const setDescriptor = async (arr, option) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     const img = new Image();
-//     img.src = URL.createObjectURL(arr[i]);
-//     const image = await faceapi
-//       .detectSingleFace(img, options)
-//       .withFaceLandmarks(true)
-//       .withFaceDescriptor();
-
-//     if (option === 'reference') {
-//       if (!image) {
-//         console.log(`Reference Image ${i + 1}: ${arr[i].name} face invalid`);
-//       } else {
-//         console.log(
-//           `Reference Image ${i + 1}: ${arr[i].name} descriptor Added `
-//         );
-//         arr[i].descriptor = image.descriptor;
-//       }
-//     }
-//     if (option === 'query') {
-//       if (!image) {
-//         console.log(`Query Image ${i + 1}: ${arr[i].name} face invalid`);
-//       } else {
-//         console.log(`Query Image ${i + 1}: ${arr[i].name} descriptor Added`);
-//         arr[i].descriptor = image.descriptor;
-//       }
-//     }
-//   }
-//   console.log(arr);
-// };
-
-// const compareDescriptor = async (refArr, queryArr) => {
-//   const refDescriptor = [];
-
-//   refArr.map((item, i) => {
-//     if (item.descriptor) {
-//       refDescriptor.push({ descriptor: item.descriptor, name: item.name });
-
-//       console.log(`Reference Image Descriptor ${i}: ${item.name} added`);
-//     }
-//   });
-//   if (refArr.length === 1) {
-//     oneReferenceToManyQuery(queryArr, refDescriptor);
-//   } else {
-//     const queryDescriptor = queryArr.map((item) => {
-//       if (item.descriptor) {
-//         return { descriptor: item.descriptor, name: item.name };
-//       }
-//     });
-
-//     await computeEuclideanDistance(refDescriptor, queryDescriptor);
-//   }
-// };
-
-// const oneReferenceToManyQuery = (arr, refDescriptor) => {
-//   const queryDescriptor = [];
-//   arr.map((item, i) => {
-//     if (item.descriptor) {
-//       queryDescriptor.push(item.descriptor);
-//       const distance = faceapi.euclideanDistance(
-//         refDescriptor[0],
-//         item.descriptor
-//       );
-//       compareDistance(distance, i);
-//     }
-//   });
-// };
-
-// const compareDistance = (distance, i) => {
-//   if (distance <= 0.43) {
-//     if (i) {
-//       console.log(
-//         `Reference Image and Query Image ${
-//           i + 1
-//         }: is the same person with the distance of ${distance}`
-//       );
-//     } else {
-//       console.log(
-//         `Reference Image and Query Image: is the same person with the distance of ${distance}`
-//       );
-//     }
-//   } else {
-//     if (i) {
-//       console.log(
-//         `Reference Image and Query Image ${
-//           i + 1
-//         }: is the NOT same person with the distance of ${distance}`
-//       );
-//     } else {
-//       console.log(
-//         `Reference Image and Query Image : is the NOT same person with the distance of ${distance}`
-//       );
-//     }
-//   }
-// };
-
-// const computeEuclideanDistance = async (arr, arr2) => {
-//   console.log(arr);
-//   console.log(arr2);
-
-//   arr.map((item, i) => {
-//     arr2.map((val, j) => {
-//       const distance = faceapi.euclideanDistance(
-//         item.descriptor,
-//         val.descriptor
-//       );
-
-//       if (distance <= 0.43) {
-//         console.log(
-//           `Reference ${i + i} ${item.name}: is the same person with query ${
-//             j + 1
-//           } ${val.name} , with the distance of ${distance}`
-//         );
-//       } else {
-//         console.log(
-//           `Reference ${i + 1} ${item.name}: is NOT the same person with query ${
-//             j + 1
-//           } ${val.name} , with the distance of ${distance}`
-//         );
-//       }
-//     });
-//   });
-// };
-
-// const compareImage = async () => {
-//   try {
-//     await setDescriptor(referenceFile, 'reference');
-//     await setDescriptor(queryFiles, 'query');
-//     await compareDescriptor(referenceFile, queryFiles);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
     faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
     faceapi.nets.faceLandmark68TinyNet.loadFromUri('./models'),
-  ]).then(() => {
+    (classifierKnn = knnClassifier.create()),
+    (tinyFaceApiNet = new faceapi.FaceLandmark68TinyNet()),
+    (mobilenetModule = await mobilenet.load()),
+  ]).then(async () => {
     document
       .getElementById('get__reference')
       .addEventListener('change', getReferenceFiles);
     document
       .getElementById('get__files')
       .addEventListener('change', getQueryFiles);
+    console.log(classifierKnn);
+    await tinyFaceApiNet.load(
+      'https://hpssjellis.github.io/beginner-tensorflowjs-examples-in-javascript/advanced-keras/face/models/face_landmark_68_tiny_model-weights_manifest.json'
+    );
+    console.log(tinyFaceApiNet);
+    console.log(mobilenetModule);
   });
 });
 
@@ -319,5 +218,17 @@ const checkBoxes = (e) => {
   }
 };
 
+const algorithmCheckboxes = (e) => {
+  const btn = e.currentTarget;
+  if (!btn.checked) return;
+  if (btn === cnnCheckbox) {
+    knnCheckbox.checked = false;
+  } else {
+    cnnCheckbox.checked = false;
+  }
+};
+
 onePersonCheckbox.addEventListener('click', checkBoxes);
 diffPersonCheckbox.addEventListener('click', checkBoxes);
+cnnCheckbox.addEventListener('click', algorithmCheckboxes);
+knnCheckbox.addEventListener('click', algorithmCheckboxes);
